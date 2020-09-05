6a3cdcdc6991ee3d76edb2432cbdf9f9661af721
Juan <jvelazquezromera@gmail.com>
2020-09-05 11:12:02 +0200

    4.20

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,14 +3,6 @@ const Blog = require('../models/blog');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
-const getTokenFrom = request => {
-    const authorization = request.get('authorization');
-    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-        return authorization.substring(7);
-    }
-    return null;
-};
-
 blogsRouter.get('/', async (request, response) => {
 
     const blogs = await Blog.find({}).populate('user');
@@ -21,7 +13,7 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
 
-    const token = getTokenFrom(request);
+    const token = request.token;
 
     let decodedToken = '';
     try {
